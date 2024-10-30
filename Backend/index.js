8cb6f21fce6475ad113a30bc37ef981ddba17700
SaroshAhmad <ahmadsarosh07@gmail.com>
2024-10-30 16:58:35 +0000

    Refactor backend server setup

--- Backend/index.js
+++ Backend/index.js
@@ -1,26 +1,23 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.status(200).json({
-        message: 'I am comming from backend server',
-        success: true
-    });
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true,
+}
 
-    // middleware
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    app.use(cookieParser());
-    const corsOptions = {
-        origin: 'http://localhost:5173',
-        credentials: true,
-    };
-    app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
-    const PORT = 5000;
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
-}); 
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
